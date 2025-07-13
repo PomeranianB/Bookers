58efa82c29aea445b95f9324b3c6ce3e756bd5f8
EC2 Default User <ec2-user@ip-172-31-14-17.ap-northeast-1.compute.internal>
2025-07-13 02:31:27 +0000

    [Add] Flash-Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,14 +28,22 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      render :new
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
+    if book.destroy
+      flash[:notice] = "Book was successfully dastroy."
+      redirect_to '/books'
+    else
+      render :new
+    end
   end
 
   private
